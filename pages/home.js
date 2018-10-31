786440e57a71801d0e8b543b963ad34c6a36b73d
Tyler Lugger <tlugger@n.io>
2018-10-31 15:33:56 -0600

    Initial commit for dashborad

--- pages/home.js
+++ pages/home.js
@@ -1,102 +1,108 @@
 import React, { Component } from 'react';
-import { Card, CardBody, Clock, Row, Col, Button } from '@nio/ui-kit';
+import { Card, CardBody, Chart, Col, Loader, Progress, Row } from '@nio/ui-kit';
 import { withPubkeeper } from '../providers/pubkeeper';
 
 class Page extends Component {
-  state = { currentTime: new Date(), historicalTime: [], brewedTime: new Date(), historicalBrewedTime: [] };
+  state = {
+    soilMoisture: false,
+    temperature: 0.0,
+    historicalTemperature: [],
+    humidity: 0.0,
+    historicalHumidity: [],
+    chartLabels: [],
+    last_watered: new Date(),
+  };
 
   componentDidMount = () => {
     const { pkClient } = this.props;
-    pkClient.addPatron('ui_scaffold.example_brew', patron => patron.on('message', this.writeDataToState));
-    pkClient.addBrewer('ui_scaffold.example_brew2', brewer => this.brewer = brewer);
-    pkClient.addPatron('ui_scaffold.example_brew2', patron => patron.on('message', this.writeDataToState2));
+    pkClient.addPatron('herbert.soil.value', patron => patron.on('message', this.handleSoil));
+    pkClient.addPatron('herbert.ht.value', patron => patron.on('message', this.handleHT));
+    pkClient.addPatron('herbert.pump.value', patron => patron.on('message', this.handlePump));
   };
 
-  writeDataToState = (data) => {
-    const { historicalTime } = this.state;
-
+  handleSoil = (data) => {
     const json = new TextDecoder().decode(data);
-    const { time } = Array.isArray(JSON.parse(json)) ? JSON.parse(json)[0] : JSON.parse(json);
-    const currentTime = new Date(time);
-    historicalTime.unshift(currentTime.toLocaleString());
-    this.setState({ currentTime, historicalTime });
+    const { soil_moist: isMoist } = JSON.parse(json)[0];
+    this.setState({ soilMoisture: isMoist });
   };
 
-  writeDataToState2 = (data) => {
-    const { historicalBrewedTime } = this.state;
-
+  handleHT = (data) => {
+    const { historicalTemperature, historicalHumidity, chartLabels } = this.state;
     const json = new TextDecoder().decode(data);
-    const { newBrewedTime } = Array.isArray(JSON.parse(json)) ? JSON.parse(json)[0] : JSON.parse(json);
-    const brewedTime = new Date(newBrewedTime);
-    historicalBrewedTime.unshift(brewedTime.toLocaleString());
-    this.setState({ brewedTime, historicalBrewedTime });
+    const { temperature: temp, humidity: humid } = JSON.parse(json)[0];
+    historicalTemperature.push(temp.toFixed(4));
+    historicalHumidity.push(humid.toFixed(4));
+    chartLabels.push('');
+    this.setState({ temperature: temp, humidity: humid, historicalTemperature, historicalHumidity, chartLabels });
   };
 
-  brewCurrentTimestamp = () => {
-    this.brewer.brewJSON([{ newBrewedTime: new Date() }]);
+  handlePump = (data) => {
+    const json = new TextDecoder().decode(data);
+    const { last_watered: watered } = JSON.parse(json)[0];
+    const water_time = new Date(watered);
+    this.setState({ last_watered: water_time });
   };
+
+  // brewCurrentTimestamp = () => {
+  //   this.brewer.brewJSON([{ newBrewedTime: new Date() }]);
+  // };
 
   render() {
-    const { currentTime, historicalTime, brewedTime, historicalBrewedTime } = this.state;
+    const { soilMoisture, temperature, humidity, historicalHumidity, historicalTemperature, chartLabels, last_watered } = this.state;
 
     return (
       <Card>
         <CardBody className="p-3">
-          <h2 className="m-0">UI Scaffold / Pubkeeper Demo</h2>
-          Sending signals to and receiving signals from nio services using the Pubkeeper javascript client.
+          <h2 className="m-0">Herb(ert) the Automated Desk Plant</h2>
+          A Pubkeeper powered dashboard for the status of Tyler's connected desk plant with automated irrigation.
           <hr />
           <Row>
-            <Col md="4">
-              <b>The Pubkeeper Provider is a HoC that uses React&apos;s Context API to:</b>
-              <ul>
-                <li>Connect to the Pubkeeper Server</li>
-                <li>Publish the time every second to <i>ui_scaffold.example_brew</i></li>
-                <li>Inject the connected pkClient via the <i>withPubkeeper</i> method</li>
-              </ul>
+            <Col md="3" className="text-center mb-3 text-nowrap">
+              <b>Plant Health</b>
+            </Col>
+            <Col md="5" className="text-center mb-3 text-nowrap">
+              <b>Plant Habitat</b>
             </Col>
             <Col xs="12" className="d-inline-block d-md-none">
               <hr />
             </Col>
-            <Col md="4">
-              <b>The left side uses the pkClient to:</b>
-              <ul>
-                <li>Create a Patron of <i>ui_scaffold.example_brew</i></li>
-                <li>Assign inbound signals on that topic to an event handler <i>writeDataToState</i></li>
-                <li>Update the Clock and historical array based on updated local state</li>
-              </ul>
+            <Col md="1" className="text-center mb-3 text-nowrap">
+              <b>Soil Moisture</b>
             </Col>
             <Col xs="12" className="d-inline-block d-md-none">
               <hr />
             </Col>
-            <Col md="4">
-              <b>The right side uses the pkClient to:</b>
-              <ul>
-                <li>Create a new Brewer <i>and</i> new Patron for topic <i>ui_scaffold.example_brew2</i></li>
-                <li>Brew the current time when you click the button</li>
-                <li>Assign inbound signals on that topic to an event handler <i>writeDataToState2</i></li>
-              </ul>
+            <Col md="3" className="text-center mb-3 text-nowrap">
+              <b>Plant Irrigation</b>
             </Col>
           </Row>
           <hr className="my-3" />
           <Row>
             <Col md="3" sm="6" className="text-center mb-3 text-nowrap">
-              <Clock value={currentTime} />
+              <img src={soilMoisture ? "/images/plant.svg" : "/images/plant-wilt.svg"} />
             </Col>
-            <Col md="3" sm="6">
-              <div id="demo_output">
-                {historicalTime && historicalTime.map(h => (<div key={h}>{h}</div>))}
-              </div>
+            <Col md="5" sm="6" className="text-center mb-3 text-nowrap">
+              <p>
+                <b>Temperature:</b> {temperature.toFixed(2)}&ordm;F <b>Humidty:</b> {humidity.toFixed(2)}%
+              </p>
+              <Chart
+                title=""
+                type="line"
+                data={{ "labels": chartLabels, "datasets": [{ "values": historicalTemperature }, { "values": historicalHumidity }] }}
+                show_dots={false}
+                heatline
+                region_fill
+              />
             </Col>
             <Col xs="12" className="d-inline-block d-md-none">
               <hr />
             </Col>
-            <Col md="3" sm="6" className="text-center mb-3 text-nowrap">
-              <Clock value={brewedTime} />
+            <Col md="1" sm="6" className="text-center mb-3 text-nowrap">
+              <Loader color={soilMoisture ? 'success' : 'warning'} />
             </Col>
-            <Col md="3" sm="6">
-              <Button block color="primary" onClick={() => this.brewCurrentTimestamp()}>Brew Current Time</Button>
-              <div id="demo_output_2">
-                {historicalBrewedTime && historicalBrewedTime.map((h, i) => (<div key={i}>{h}</div>))}
+            <Col md="3" sm="6" className="text-center mb-3 text-nowrap">
+              <div>
+                <b>Last Watered: </b>{last_watered.toLocaleString()}
               </div>
             </Col>
           </Row>
