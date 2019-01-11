a4e21efc0e1b8a62ff3f83094aa75b0255b2f0fb
Tyler Lugger <tlugger@n.io>
2019-01-11 10:48:20 -0700

    Add additional components for v2

--- pages/home.js
+++ pages/home.js
@@ -12,6 +12,9 @@ class Page extends Component {
     chartLabels: Array(30).fill(''),
     last_watered: [],
     pump_status: false,
+    count: 0,
+    state: 'red',
+    tweet: '',
   };
 
   componentDidMount = () => {
@@ -20,6 +23,8 @@ class Page extends Component {
     pkClient.addPatron('herbert.ht.value', patron => patron.on('message', this.handleHT));
     pkClient.addPatron('herbert.pump.value', patron => patron.on('message', this.handlePump));
     pkClient.addPatron('herbert.pump.status', patron => patron.on('message', this.handlePumpStatus));
+    pkClient.addPatron('pine.christmas.count', patron => patron.on('message', this.handleChristmas));
+    pkClient.addPatron('handle_blink', patron => patron.on('message', this.handleSpotify));
   };
 
   handleSoil = (data) => {
@@ -30,7 +35,7 @@ class Page extends Component {
   };
 
   handleHT = (data) => {
-    const { historicalTemperature, historicalHumidity, chartLabels } = this.state;
+    const { historicalTemperature, historicalHumidity } = this.state;
     const json = new TextDecoder().decode(data);
     const { temperature: temp, humidity: humid } = JSON.parse(json)[0];
 
@@ -56,93 +61,202 @@ class Page extends Component {
     this.setState({ pump_status: val });
   };
 
+  handleChristmas = (data) => {
+    const json = new TextDecoder().decode(data);
+    const { count: total, state: color, tweet: text } = JSON.parse(json)[0];
+    this.setState({ count: total, state: color, tweet: text });
+  }
+
+  handleSpotify = (data) => {
+    const json = new TextDecoder().decode(data);
+    const { device, album } = JSON.parse(json)[0];
+    this.setState({ device: device.name, art: album.url });
+  }
+
   render() {
-    const { soilMoisture, temperature, humidity, historicalHumidity, historicalTemperature, chartLabels, last_watered, pump_status } = this.state;
+    const { 
+      soilMoisture,
+      temperature,
+      humidity,
+      historicalHumidity,
+      historicalTemperature,
+      chartLabels,
+      last_watered,
+      pump_status,
+      count,
+      state,
+      tweet,
+      device,
+      art,
+    } = this.state;
 
     return (
-      <Card>
-        <CardBody className="p-3">
-          <Row>
-            <Col md="11">
-              <h2 className="m-0">Herb(ert) the Automated Desk Plant</h2>
-              <p>
-                A Pubkeeper powered dashboard for the status of Tyler's connected desk plant with automated irrigation.
-              </p>
-            </Col>
-            <Col md="1" className="text-center">
-              <img className="cilantro-image" src="/images/cilantro.svg" alt="" />
-            </Col>
-          </Row>
-          <hr />
-          <Row>
-            <Col md="2" className="text-center mb-3">
-              <Card className="plant-card">
-                <CardBody>
-                  <b>Plant Health</b>
-                  <hr />
-                  <p>
-                    <br />
-                  </p>
-                  <img className="plant-image" src={soilMoisture >= 2.601 ? '/images/plant.svg' : '/images/plant-wilt.svg'} alt="" />
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="5" className="text-center mb-3">
-              <Card className="plant-card">
-                <CardBody>
-                  <b>Plant Habitat</b>
-                  <hr />
-                  <p>
-                    <b>Temperature:</b> {temperature.toFixed(2)}&ordm;C <b>Humidty:</b> {humidity.toFixed(2)}%
-                  </p>
-                  <Chart
-                    title=""
-                    type="line"
-                    data={{ "labels": chartLabels, "datasets": [{ "values": historicalHumidity }, { "values": historicalTemperature }] }}
-                    show_dots={false}
-                    heatline
-                    region_fill
-                  />
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="2" className="text-center mb-3">
-              <Card className="plant-card">
-                <CardBody>
-                  <b>Soil Moisture</b>
-                  <hr />
-                  <b>Value: </b>{soilMoisture.toFixed(3)}
-                  <br />
-                  <div className="loader-div">
-                    <Loader color={soilMoisture >= 2.601 ? 'success' : 'warning'} />
-                  </div>
-                  <br />
-                  <br />
-                </CardBody>
-              </Card>
-            </Col>
-            <Col md="3" className="text-center mb-3">
-              <Card className="plant-card">
-                <CardBody>
-                  <b>Plant Irrigation</b>
-                  <hr />
-                  <b>Last Watered: </b>{last_watered}
-                  <br />
-                  <br />
-                  <br />
-                  <div>
-                    <ToggleButton
-                      inactiveLabel={<b>Off</b>}
-                      activeLabel={<b>On</b>}
-                      value={pump_status}
+      <div>
+        <Card>
+          <CardBody className="p-3">
+            <Row>
+              <Col md="11">
+                <h2 className="m-0">Herb(ert) the Automated Desk Plant</h2>
+                <p>
+                  A Pubkeeper powered dashboard for the status of Tyler's connected desk plant with automated irrigation.
+                </p>
+              </Col>
+              <Col md="1" className="text-center">
+                <img className="card-image" src="/images/cilantro.svg" alt="" />
+              </Col>
+            </Row>
+            <hr />
+            <Row>
+              <Col md="2" className="text-center mb-3">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Plant Health</b>
+                    <hr />
+                    <p>
+                      <br />
+                    </p>
+                    <img className="plant-image" src={soilMoisture >= 2.601 ? '/images/plant.svg' : '/images/plant-wilt.svg'} alt="" />
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="5" className="text-center mb-3">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Plant Habitat</b>
+                    <hr />
+                    <p>
+                      <b>Temperature:</b> {temperature.toFixed(2)}&ordm;C <b>Humidty:</b> {humidity.toFixed(2)}%
+                    </p>
+                    <Chart
+                      title=""
+                      type="line"
+                      data={{ "labels": chartLabels, "datasets": [{ "values": historicalHumidity }, { "values": historicalTemperature }] }}
+                      show_dots={false}
+                      heatline
+                      region_fill
                     />
-                  </div>
-                </CardBody>
-              </Card>
-            </Col>
-          </Row>
-        </CardBody>
-      </Card>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="2" className="text-center mb-3">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Soil Moisture</b>
+                    <hr />
+                    <b>Value: </b>{soilMoisture.toFixed(3)}
+                    <br />
+                    <div className="loader-div">
+                      <Loader color={soilMoisture >= 2.601 ? 'success' : 'warning'} />
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="3" className="text-center mb-3">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Plant Irrigation</b>
+                    <hr />
+                    <b>Last Watered: </b>{last_watered}
+                    <br />
+                    <br />
+                    <br />
+                    <div>
+                      <ToggleButton
+                        inactiveLabel={<b>Off</b>}
+                        activeLabel={<b>On</b>}
+                        value={pump_status}
+                      />
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+        <hr />
+        <Card>
+          <CardBody className="p-3">
+            <Row>
+              <Col md="11">
+                <h2 className="m-0">#Christmas Counter</h2>
+                <p>
+                  A live display of the number of counted tweets containing '#Christmas'. This number controls a connected LED on Tyler's desk.
+                </p>
+              </Col>
+              <Col md="1" className="text-center">
+                <img className="card-image" src="/images/santa.svg" alt="" />
+              </Col>
+            </Row>
+            <hr />
+            <Row>
+              <Col md="3" className="text-center">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Count</b>
+                    <hr />
+                    <h3>{count}</h3>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="6" className="text-center">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Tweet</b>
+                    <hr />
+                    <h6>{tweet}</h6>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="3" className="text-center">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Color</b>
+                    <hr />
+                    <img className="light-image" src={state === 'red' ? '/images/red-light.svg' : '/images/green-light.svg'} alt="" />
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+        <hr />
+        <Card>
+          <CardBody className="p-3">
+            <Row>
+              <Col md="11">
+                <h2 className="m-0">Spotify Authoirzation Monitor</h2>
+                <p>
+                  Tyler's Spotify usage including devices currently accessing my account.
+                </p>
+              </Col>
+              <Col md="1" className="text-center">
+                <img className="card-image" src="/images/spotify.svg" alt="" />
+              </Col>
+            </Row>
+            <hr />
+            <Row>
+              <Col md="6" className="text-center">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Device</b>
+                    <hr />
+                    <h3>{device || 'None'}</h3>
+                  </CardBody>
+                </Card>
+              </Col>
+              <Col md="6" className="text-center">
+                <Card className="plant-card">
+                  <CardBody>
+                    <b>Album</b>
+                    <hr />
+                    <img className="card-image" src={art} alt="" />
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 }
